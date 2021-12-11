1a78f74cc981db9df31046b8c9783dca01a14165
Oskari Kotajärvi <okotajarvi7@gmail.com>
2021-12-11 21:00:07 +0200

    Added an entrypoint for the app

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(morgan('common'));
 
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello world' });
+});
+
 app.get('/ping', (req, res) => {
     res.json({ message: 'pong' });
 });
